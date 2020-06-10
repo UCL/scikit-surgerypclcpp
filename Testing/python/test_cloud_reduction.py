bb7ab7e099ae3ca2b975b1c34b44441f7fa4ab37
Matt Clarkson <m.clarkson@ucl.ac.uk>
2020-06-10 12:05:00 +0100

    Issue #17: Quick sanity check test for pass through filter and radius removal filter.

--- Testing/python/test_cloud_reduction.py
+++ Testing/python/test_cloud_reduction.py
@@ -20,5 +20,13 @@ def test_cloud_reduction():
     outlier_reduced_surface = sks.remove_outlier_points(voxel_reduced_surface, 50, 1.0)
     six.print_("outlier_reduced_surface.shape=" + str(outlier_reduced_surface.shape))
 
+    pass_through_filtered = sks.pass_through_filter(outlier_reduced_surface, 'z', -1, -0.05, True)
+    six.print_("pass_through_filtered.shape=" + str(pass_through_filtered.shape))
+
+    radius_filtered = sks.radius_removal_filter(pass_through_filtered, 0.01, 1)
+    six.print_("radius_filtered.shape=" + str(radius_filtered.shape))
+
     assert voxel_reduced_surface.shape[0] < source_points.shape[0]
     assert outlier_reduced_surface.shape[0] < voxel_reduced_surface.shape[0]
+    assert radius_filtered.shape[0] < outlier_reduced_surface.shape[0]
+    assert radius_filtered.shape[0] < pass_through_filtered.shape[0]
